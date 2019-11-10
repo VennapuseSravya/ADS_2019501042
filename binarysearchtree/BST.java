20e5aa8abe821015dacb138a21741fb891a71e1a
VennapuseSravya <vsry0897@msitprogram.net>
2019-11-10 16:06:37 +0530

    assignments

--- binarysearchtree/BST.java
+++ binarysearchtree/BST.java
@@ -1,148 +1,90 @@
-// @ author Sravya
+import java.util.Arrays;
 
-public class BST<Key extends Comparable<Key>, Value> {
-
-    Key[] keys;
-    Value[] values;
-    int[] leftIndices;
-    int[] rightIndices;
-    int[] sizes;
-
+class BST<Key extends Comparable<Key>, Value> {
+    Key[] key;
+    Value[] val;
+    int[] leftIndex;
+    int[] rightIndex;
+    int[] size;
 
     public BST() {
-        this.keys = (Key[]) new Comparable[20];
-        this.values = (Value[]) new Object[20];
-        this.leftIndices = new int[20];
-        this.rightIndices = new int[20];
-        this.sizes = new int[20];
-
-        for (int i = 0; i < 20; i++) {
-            leftIndices[i] = -1;
-            rightIndices[i] = -1;
+        this.key = (Key[]) new Comparable[20];
+        this.val = (Value[]) new Comparable[20];
+        this.leftIndex = new int[20];
+        this.rightIndex = new int[20];
+        this.size = new int[20];
+       
+        for (int i = 0; i < 10; i++) {
+            leftIndex[i] = -1;
+            rightIndex[i] = -1;
         }
-
     }
 
-    // return size of the node
-    public int size(int keyIndex) {
-        if (keyIndex == -1) {
+    public int size(int index) {
+        if (index == -1) {
             return 0;
         } else {
-            return sizes[keyIndex];
+            return size[index];
         }
     }
+
     public int size() {
-        return sizes[0];
+        return size[0];
     }
-
-
-
-    // isEmpty()
-    public boolean isEmpty() {
-        return keys[0] == null;
-    }
-
-    public boolean contains(Key key) {
-        for (Key eachKey : keys) {
-            if(eachKey.compareTo(key) == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 
     public Value get(Key key) {
         return get(0, key);
     }
 
-    public Value get(int keyIndex, Key keyToCompare) {
-
-        if (keyIndex < 0) {
+    public Value get(int index, Key k) {
+        if (index < 0) {
             return null;
         }
-
-        int cmp = keyToCompare.compareTo(keys[keyIndex]);
-
+        int cmp = k.compareTo(key[index]);
         if (cmp < 0) {
-            
-            return get(leftIndices[keyIndex], keyToCompare); // goes left
-
+            return get(leftIndex[index], k);
         } else if (cmp > 0) {
-            
-            return get(rightIndices[keyIndex], keyToCompare); // goes right
-
+            return get(rightIndex[index], k);
         } else {
-            return values[keyIndex];
+            return val[index];
         }
     }
 
-
-    public void put(Key key, Value val) {
-        put(0, key, val);
+    public void put(Key k, Value v) {
+        put(0, k, v);
     }
 
-
-    public int put(int keyIndex, Key keyToCompare, Value val) {
-        if (keyIndex  == -1 || keys[keyIndex] == null) {
-            // if the root itself is null
-            // the new one becomes the root
-
-            keyIndex = size();
-            
-            keys[keyIndex] = keyToCompare;
-            values[keyIndex] = val;
-            sizes[keyIndex] = 1;
-            // System.out.println(keyIndex + "---------" + (String)keyToCompare + "--- On an empty BST");
-
-            return keyIndex;
+    public int put(int index, Key k, Value v) {
+        if (index == -1 || key[index] == null) {
+            index = size();
+            key[index] = k;
+            val[index] = v;
+            size[index] = 1;
+            return index;
         }
-
-        int cmp = keyToCompare.compareTo(keys[keyIndex]);
-
-        // if less than root, goes to its left indices
-        // RECURSIVE
-        if(cmp < 0) {
-            leftIndices[keyIndex] = put(leftIndices[keyIndex],  keyToCompare, val);
-
-        // if greater than root, goes to its right indices
-        // RECURSIVE        
+        int cmp = k.compareTo(key[index]);
+        if (cmp < 0) {
+            leftIndex[index] = put(leftIndex[index], k, v);
         } else if (cmp > 0) {
-
-            rightIndices[keyIndex] = put(rightIndices[keyIndex], keyToCompare, val);
-
-        // Equals says, key already existing
-        // Just updates its value.
+            rightIndex[index] = put(rightIndex[index], k, v);
         } else {
-            values[keyIndex] = val;
+            val[index] = v;
         }
-
-        // At this stage, with all recursions
-        // recursion increaments size to every key's size in the path.
-        // size() method returns 0 on an unoccupied index(i.e., -1).
-        sizes[keyIndex] = size(leftIndices[keyIndex]) + size(rightIndices[keyIndex]) + 1; 
-
-        return keyIndex;
+        size[index] = size(leftIndex[index]) + size(rightIndex[index]) + 1;
+        return index;
     }
-
 
     public static void main(String[] args) {
-        BST<String, Integer> bst = new BST<String, Integer>();
-
-        bst.put("H",0);
-        bst.put("A",1);
-        bst.put("R",2);
-        bst.put("I",3);
-        bst.put("K",4);
-        bst.put("A",5);
-        bst.put("R",6);
-        bst.put("E",7);
-        bst.put("D",8);
-        bst.put("D",9);
-        bst.put("Y",10);
-        bst.put("G",11);
-        bst.put("R",12);
-
-        System.out.println(bst.get("Y"));
+        BST<String, Integer> b = new BST<String, Integer>();
+        b.put("b", 0);
+        b.put("s", 1);
+        b.put("t", 2);
+        b.put("a", 3);
+        b.put("r", 4);
+        b.put("r", 5);
+        b.put("a", 6);
+        b.put("y", 7);
+        System.out.println(b.get("y"));
+        b.toString();
     }
 }
